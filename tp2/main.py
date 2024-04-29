5202b870d769cab44ebaeeb3a60ec2b777237b15
Matias Bartellone <matias.bartellone@gmail.com>
2024-04-28 21:33:12 -0300

    agrego algoritmo nuevo

--- tp2/main.py
+++ tp2/main.py
@@ -1,6 +1,44 @@
 import sys
 from leer_prueba import leer_prueba
 
+def algoritmo_nuevo(x,f,n):
+    if n == 0:
+        return 0
+    
+    OPT = [[0] * (n+1) for _ in range(n+1)]
+    OPT[1][1] = min(f[1],x[1])
+    
+    for m in range(2, n+1):
+        for j in range(1, m+1):
+            OPT[m][j] = max(OPT[m][j-1], (OPT[m-j][m-j] + min(f[j], x[m])))
+
+    return OPT
+
+def reconstruccion_nuevo(OPT, n):
+    ataques = []
+    max_bajas = OPT[n][n]
+    m, j = n,n
+    while True:
+        if j == 0:
+            break
+        if OPT[m][j] != OPT[m][j-1]:
+            ataques.append(m)
+            m = m-j
+            j = m
+        else:
+            j -=1
+
+    reco = []
+
+    for i in range(1, n+1):
+        if len(ataques) > 0 and ataques[-1] == i:
+            reco.append("ATACAR")
+            ataques.pop(-1)
+        else:
+            reco.append("DESCANSAR")
+    return reco, max_bajas
+
+
 def algoritmo_llorens(x, f, n): #????
     if n == 0:
         return 0
@@ -60,8 +98,8 @@ def main():
 
     n,x,f = leer_prueba(archivo_prueba)
 
-    OPT = algoritmo_llorens2(x, f, n)
-    reco, max_bajas = reconstruccion(OPT, n)
+    OPT = algoritmo_nuevo(x, f, n)
+    reco, max_bajas = reconstruccion_nuevo(OPT, n)
 
 
     print("Estrategia: ", reco)
