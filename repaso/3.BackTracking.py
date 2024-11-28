541f3a19a79db47aad6e78d580fd5008f905fcc6
thiagopservian <tpacheco@fi.uba.ar>
2024-11-27 21:22:40 -0300

    test dominating

--- repaso/3.BackTracking.py
+++ repaso/3.BackTracking.py
@@ -1,3 +1,6 @@
+from grafo import Grafo
+from vertice import Vertice
+
 def _dominating_set(grafo, vertices, actual, solucion_parcial, suma_parcial, solucion_actual, suma_actual): 
     if es_dominating_set(grafo, solucion_parcial):
         if suma_actual < suma_parcial: 
@@ -7,7 +10,7 @@ def _dominating_set(grafo, vertices, actual, solucion_parcial, suma_parcial, sol
     if actual >= len(vertices) or suma_parcial >= suma_actual:
         return solucion_actual, suma_actual
     solucion_parcial.append(vertices[actual])
-    solucion_actual, suma_actual = _dominating_set(grafo, vertices, actual+1, solucion_parcial, suma_parcial, vertices[actual].valor, solucion_actual, suma_actual)
+    solucion_actual, suma_actual = _dominating_set(grafo, vertices, actual+1, solucion_parcial, suma_parcial + vertices[actual].valor, solucion_actual, suma_actual)
     solucion_parcial.pop()
     solucion_actual, suma_actual = _dominating_set(grafo, vertices, actual+1, solucion_parcial, suma_parcial + vertices[actual].valor, solucion_actual, suma_actual)
     return solucion_actual, suma_actual
@@ -23,4 +26,29 @@ def es_dominating_set(grafo, solucion):
     return len(grafo) == len(cubierto)
     
 def dominating_set(grafo):
-    return _dominating_set(grafo, grafo.vertices(), 0, [], 0, None, float('inf'))
+    return _dominating_set(grafo, grafo.obtener_vertices(), 0, [], 0, None, float('inf'))
+
+grafo = Grafo()
+
+A = Vertice("A", 1)
+B = Vertice("B", 2)
+C = Vertice("C", 3)
+D = Vertice("D", 4)
+
+# Agregar los vértices al grafo
+grafo.agregar_vertice(A)
+grafo.agregar_vertice(B)
+grafo.agregar_vertice(C)
+grafo.agregar_vertice(D)
+
+# Agregar las aristas
+grafo.agregar_arista(A, B)
+grafo.agregar_arista(A, C)
+grafo.agregar_arista(B, D)
+
+# Encontrar el Dominating Set
+solucion, peso = dominating_set(grafo)
+
+# Imprimir resultados
+print("Dominating Set:", solucion)
+print("Peso mínimo:", peso)
---
+++ repaso/vertice.py
@@ -0,0 +1,7 @@
+class Vertice:
+    def __init__(self, nombre, valor):
+        self.nombre = nombre
+        self.valor = valor
+
+    def __repr__(self):
+        return self.nombre
